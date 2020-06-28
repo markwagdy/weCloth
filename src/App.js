a21b961170f3a287fd43d2b3489b9360ccb9c0e5
Mark Wagdy <16P8146@eng.asu.edu.eg>
2020-06-28 06:17:52 +0200

    Header Component added with styling and log

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Route ,Switch} from 'react-router-dom';
 import './App.css';
 import HomePage from './Pages/HomePage/homepage.component';
 import ShopPage from './Pages/Shop/shop.component';
-const hats=()=>
-{
-  return <div>
-    <h1>Hats Page</h1>        
-  </div>
-}
+import Header from './Components/Header/header.component';
+
 function App() {
   return (
     <div >
+    <Header/>
+    <Switch>
     <Route exact path='/' component={HomePage}/>
-    <Route path='/hats' component={ShopPage}/>
+    <Route path='/shop' component={ShopPage}/>
+    </Switch>
     </div>
   );
 }
---
+++ src/Components/Header/header.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './header.styles.scss';
+import {Link} from 'react-router-dom';
+import {ReactComponent as Logo} from '../../assets/original.svg';
+
+const Header =()=>
+(
+    <div className='header'>
+        <Link className='logo-container' to="/">
+        <Logo className='logo'/>
+        </Link>
+        <div className='options'>
+            <Link className='option'>SHOP</Link>
+            <Link className='option'>CONTACT</Link>
+
+         </div>
+         </div>
+
+)
+export default Header;
