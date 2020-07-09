119e7521c64043c1faf5e8091cc4e64bf42ab560
Mark Wagdy <16P8146@eng.asu.edu.eg>
2020-07-09 15:53:27 +0200

    modified in rendering sign in after signing in and actio types for best practice

--- src/Redux/user/user.reducer.jsx
+++ src/Redux/user/user.reducer.jsx
@@ -1,10 +1,11 @@
+import {userActionTypes} from './user.types';
 const Intiall_State={
 currentUser:null
 };
 const userReducer=(state =Intiall_State,action)=>{
     switch(action.type)
     {
-        case 'SET_CURRENT_USER':
+        case userActionTypes.SET_CURRENT_USER:
         return {
             ...state,
             currentUser:action.payload
